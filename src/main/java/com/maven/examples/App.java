9887f32eb3cd0e72bd526d908ef7402c520df761
esha <eshaagrawal277@gmail.com>
2022-11-09 23:05:01 +0530

    check

---
+++ src/main/java/com/maven/examples/App.java
@@ -0,0 +1,14 @@
+package com.maven.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/maven/examples/AppTest.java
@@ -0,0 +1,17 @@
+package com.maven.examples;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void test_junit(){
+        String s1 = "I do not match expected";
+        String s2 = "This is test case in class";
+        assertEquals("This is test case in class", s1);
+    }
+}
